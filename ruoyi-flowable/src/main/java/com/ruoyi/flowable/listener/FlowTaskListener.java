84e1751f1f9932fea159636ae26c2aa50921661c
muyu <1902982537@qq.com>
2026-03-01 10:58:57 +0800

    fix: use onTaskCompleted with userId in FlowTaskListener to populate claim_user_id

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/listener/FlowTaskListener.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/listener/FlowTaskListener.java
@@ -1,5 +1,7 @@
 package com.ruoyi.flowable.listener;
 
+import com.ruoyi.common.utils.spring.SpringUtils;
+import com.ruoyi.flowable.service.IFlowTeamService;
 import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.delegate.TaskListener;
 import org.flowable.task.service.delegate.DelegateTask;
@@ -18,15 +20,41 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-public class FlowTaskListener implements TaskListener{
+public class FlowTaskListener implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
+        String event = delegateTask.getEventName();
+        String taskId = delegateTask.getId();
+        String procInstId = delegateTask.getProcessInstanceId();
+        String nodeKey = delegateTask.getTaskDefinitionKey();
+        String nodeName = delegateTask.getName();
 
-        log.info("任务监听器:{}", delegateTask);
-        // TODO  获取事件类型 delegateTask.getEventName(),可以通过监听器给任务执行人发送相应的通知消息
+        log.info("任务监听器触发，事件: {}, 任务ID: {}, 流程实例ID: {}", event, taskId, procInstId);
 
+        try {
+            IFlowTeamService flowTeamService = SpringUtils.getBean(IFlowTeamService.class);
 
+            if (EVENTNAME_CREATE.equals(event)) {
+                // 任务创建时：注入班组成员为候选人 + 写 task_node_execution
+                flowTeamService.injectTeamCandidates(taskId, procInstId, nodeKey, nodeName);
+            } else if (EVENTNAME_COMPLETE.equals(event)) {
+                // 任务完成时：更新 task_node_execution 状态，并记录执行人
+                // delegateTask.getAssignee() 在 complete 事件时即为实际完成任务的用户ID
+                String assigneeStr = delegateTask.getAssignee();
+                Long assigneeId = null;
+                if (assigneeStr != null && !assigneeStr.isEmpty()) {
+                    try {
+                        assigneeId = Long.parseLong(assigneeStr);
+                    } catch (NumberFormatException ex) {
+                        log.warn("无法解析 assignee 为 userId: {}", assigneeStr);
+                    }
+                }
+                flowTeamService.onTaskCompleted(taskId, "completed", "", assigneeId);
+            }
+        } catch (Exception e) {
+            log.error("任务监听器处理异常，事件: {}, 任务ID: {}", event, taskId, e);
+        }
     }
 
 }
